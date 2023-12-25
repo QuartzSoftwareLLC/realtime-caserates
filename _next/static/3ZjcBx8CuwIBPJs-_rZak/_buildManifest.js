4b9cf9ad89f8c4d651eaa3f574f0b378adfb7453
Jacob Clarke <jacobclarke718@gmail.com>
2023-12-24 18:17:58 -0600

    export

---
+++ _next/static/3ZjcBx8CuwIBPJs-_rZak/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/402-7cc05884649ab5ae.js","static/chunks/pages/index-ba58296015e6b4f9.js"],"/_error":["static/chunks/pages/_error-5607e64d58853244.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
